61c9eedef2c42a1c12c5d5a26e9014f1443bb339
hyunjoolee <hlee748@gmail.com>
2023-07-25 14:59:28 +0900

    homepage - 와이어프레임

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomePage extends StatefulWidget{
   const HomePage({super.key});
@@ -11,8 +12,38 @@ class HomePage extends StatefulWidget{
 class HomePageState extends State<HomePage>{
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          const SliverAppBar(
+            backgroundColor: Colors.white,
+            title: Text('Record', style: TextStyle(color: Colors.black),),
+          ),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                    return Card(
+                      margin: const EdgeInsets.all(10),
+                      child: Container(
+                        color: Colors.yellow,
+                        height: 80,
+                        alignment: Alignment.center,
+                        child: Text('기록 ${index}'),
+                      ),
+                    );
+                  },
+                  childCount: 10
+              )
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            context.go('/recording');
+          },
+        backgroundColor: Colors.black,
+        child: const Icon(Icons.navigate_next),
+      ),
     );
   }
 }
--- lib/presentation/record/recording_page.dart
+++ lib/presentation/record/recording_page.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class RecordingPage extends StatefulWidget{
+class RecordingPage extends StatefulWidget {
   const RecordingPage({super.key});
 
   @override
   State createState() => RecordingPageState();
 }
 
-class RecordingPageState extends State<RecordingPage>{
+class RecordingPageState extends State<RecordingPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-
+      color: Colors.pink,
     );
   }
 }
