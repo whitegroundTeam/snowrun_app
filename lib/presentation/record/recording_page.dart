595c269ba4b6a8dd5c509edddf619ba58cb585f9
hyunjoolee <hlee748@gmail.com>
2023-07-26 17:16:17 +0900

    지도 페이지 목업

--- lib/presentation/record/recording_page.dart
+++ lib/presentation/record/recording_page.dart
@@ -20,17 +20,56 @@ class RecordingPageState extends State<RecordingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MapWidget(
-        key: const ValueKey('mapWidget'),
-        resourceOptions: ResourceOptions(accessToken: 'pk.eyJ1Ijoid2hpdGVncm91bmQiLCJhIjoiY2xrYW96N2RkMDd5eTNyanM2ZmVjZ2E4ciJ9.DFZG8kD1ahfSc1sk-cN-bw'),
-        onMapCreated: _onMapCreated,
-        cameraOptions: CameraOptions(
-          center: Point(
-            coordinates: Position(6.0033416748046875,
-              43.70908256335716,)
-          ).toJson(),
-          zoom: 10.0
-        ),
+      body: Stack(
+        children: [
+          MapWidget(
+            key: const ValueKey('mapWidget'),
+            resourceOptions: ResourceOptions(accessToken: 'pk.eyJ1Ijoid2hpdGVncm91bmQiLCJhIjoiY2xrYW96N2RkMDd5eTNyanM2ZmVjZ2E4ciJ9.DFZG8kD1ahfSc1sk-cN-bw'),
+            onMapCreated: _onMapCreated,
+            cameraOptions: CameraOptions(
+                center: Point(
+                    coordinates: Position(6.0033416748046875,
+                      43.70908256335716,)
+                ).toJson(),
+                zoom: 10.0
+            ),
+          ),
+          Positioned(
+            left: 20.0,
+            right: 20.0,
+            bottom: 40.0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12)
+                ),
+                height: 200,
+                width: MediaQuery.of(context).size.width,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('현재구역', style: TextStyle(fontSize: 24),),
+                      const SizedBox(height: 24,),
+                      GestureDetector(
+                        onTap: (){},
+                        child: Container(
+                          padding: const EdgeInsets.all(0.0),
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(100)
+                          ),
+                          child: const Icon(Icons.stop, size:80,color: Colors.white,),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+          ),
+        ]
       ),
     );
   }
