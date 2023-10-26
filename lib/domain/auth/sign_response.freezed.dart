6c80e74482e50c05375a28828954f2bc6b606182
chokwanghwan <kwanghwancho92@gmail.com>
2023-10-26 23:39:42 +0900

    인증쪽 repository 작업 가능한 부분까지 해둠

---
+++ lib/domain/auth/sign_response.freezed.dart
@@ -0,0 +1,154 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'sign_response.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$SignResponse {
+  StringVO get authToken => throw _privateConstructorUsedError;
+  BooleanVO get isNewUser => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $SignResponseCopyWith<SignResponse> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $SignResponseCopyWith<$Res> {
+  factory $SignResponseCopyWith(
+          SignResponse value, $Res Function(SignResponse) then) =
+      _$SignResponseCopyWithImpl<$Res, SignResponse>;
+  @useResult
+  $Res call({StringVO authToken, BooleanVO isNewUser});
+}
+
+/// @nodoc
+class _$SignResponseCopyWithImpl<$Res, $Val extends SignResponse>
+    implements $SignResponseCopyWith<$Res> {
+  _$SignResponseCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? authToken = null,
+    Object? isNewUser = null,
+  }) {
+    return _then(_value.copyWith(
+      authToken: null == authToken
+          ? _value.authToken
+          : authToken // ignore: cast_nullable_to_non_nullable
+              as StringVO,
+      isNewUser: null == isNewUser
+          ? _value.isNewUser
+          : isNewUser // ignore: cast_nullable_to_non_nullable
+              as BooleanVO,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_SignResponseCopyWith<$Res>
+    implements $SignResponseCopyWith<$Res> {
+  factory _$$_SignResponseCopyWith(
+          _$_SignResponse value, $Res Function(_$_SignResponse) then) =
+      __$$_SignResponseCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({StringVO authToken, BooleanVO isNewUser});
+}
+
+/// @nodoc
+class __$$_SignResponseCopyWithImpl<$Res>
+    extends _$SignResponseCopyWithImpl<$Res, _$_SignResponse>
+    implements _$$_SignResponseCopyWith<$Res> {
+  __$$_SignResponseCopyWithImpl(
+      _$_SignResponse _value, $Res Function(_$_SignResponse) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? authToken = null,
+    Object? isNewUser = null,
+  }) {
+    return _then(_$_SignResponse(
+      authToken: null == authToken
+          ? _value.authToken
+          : authToken // ignore: cast_nullable_to_non_nullable
+              as StringVO,
+      isNewUser: null == isNewUser
+          ? _value.isNewUser
+          : isNewUser // ignore: cast_nullable_to_non_nullable
+              as BooleanVO,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_SignResponse extends _SignResponse {
+  const _$_SignResponse({required this.authToken, required this.isNewUser})
+      : super._();
+
+  @override
+  final StringVO authToken;
+  @override
+  final BooleanVO isNewUser;
+
+  @override
+  String toString() {
+    return 'SignResponse(authToken: $authToken, isNewUser: $isNewUser)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_SignResponse &&
+            (identical(other.authToken, authToken) ||
+                other.authToken == authToken) &&
+            (identical(other.isNewUser, isNewUser) ||
+                other.isNewUser == isNewUser));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, authToken, isNewUser);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_SignResponseCopyWith<_$_SignResponse> get copyWith =>
+      __$$_SignResponseCopyWithImpl<_$_SignResponse>(this, _$identity);
+}
+
+abstract class _SignResponse extends SignResponse {
+  const factory _SignResponse(
+      {required final StringVO authToken,
+      required final BooleanVO isNewUser}) = _$_SignResponse;
+  const _SignResponse._() : super._();
+
+  @override
+  StringVO get authToken;
+  @override
+  BooleanVO get isNewUser;
+  @override
+  @JsonKey(ignore: true)
+  _$$_SignResponseCopyWith<_$_SignResponse> get copyWith =>
+      throw _privateConstructorUsedError;
+}
